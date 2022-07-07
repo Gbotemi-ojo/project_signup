e27593badf08ee94a5a6d3e0edb40802c6a9a2e6
gbotemi <hommzmum@gmail.com>
2022-07-07 06:11:25 -0700

    done with forms,added password verification

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let password = document.getElementById("password")
 let confirmpassword = document.getElementById("confirmPassword")
 let errorText = document.querySelector(".errorPassword")
-let submitBtn = document.querySelector(".submitBtn")
-submitBtn.addEventListener("click",confirmpass)
-function confirmpass(){
-   if(password.value === confirmpassword.value){ 
-       errorText.textContent = ""
-   }
-   else if(password.value !== confirmpassword.value){
-       errorText.textContent = "*passwords do not match"
-   }
+confirmpassword.addEventListener("keyup",confirmed)
+function confirmed(){
+    if(password.value != confirmpassword.value){
+        errorText.textContent = "*passwords do not match" 
+    }
+    else{
+        errorText.textContent = ""
+    }
 }
+// let submitBtn = document.querySelector(".submitBtn")
+// submitBtn.addEventListener("click",confirmpass)
+// function confirmpass(){
+//    if(password.value === confirmpassword.value){ 
+//        errorText.textContent = ""
+//    }
+//    else if(password.value !== confirmpassword.value){
+//        errorText.textContent = "*passwords do not match"
+//    }
+// }
